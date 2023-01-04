4edd455263f01f9f8bf5dff180d93f996b4469e0
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-01-04 09:22:29 -0400

    chat model has created

---
+++ models/chat.js
@@ -0,0 +1,40 @@
+class Message {
+
+    constructor(uid, name, message) {
+        this.uid = uid;
+        this.name = name;
+        this.message = message
+    } 
+}
+
+export default class ChatMessages {
+
+    constructor() {
+        this.messages = [];
+        this.users = {};
+    }
+ 
+    get last10Message () {
+        this.messages = this.messages.splice(0, 10);
+        return this.messages;
+    }
+
+    get usersArray () {
+        return Object.values(this.users);
+    }
+
+    sendMessage(uid, name, message) {
+        this.messages.unshift(
+            new Message(uid, name, message)
+        );
+    }
+
+    connectUser(user) {
+        this.users[user.id] = user;
+    }
+
+
+    disconnectUser(id) {
+        delete this.users[id];
+    }
+}
--- models/index.js
+++ models/index.js
@@ -3,11 +3,13 @@ import Role from "./role.js";
 import Server from "./server.js";
 import User from "./user.js";
 import Product from './product.js'
+import ChatMessages from './chat.js'
 
 export {
     Category,
     Role,
     User,
     Server,
-    Product
+    Product,
+    ChatMessages
 }
